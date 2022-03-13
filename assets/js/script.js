8996eaeb88aecd336fe94b05c5a9407fe5107171
ffhenrique <86134993+ffhenrique@users.noreply.github.com>
2022-03-12 22:34:02 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+let sliderElement = document.getElementById('slider');
+let buttonElement = document.getElementById('button');
+
+let sizePassword = document.getElementById('valor');
+let passwoord = document.getElementById('password');
+
+let containerPassword = document.getElementById('container-password');
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@&';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function () {
+   sizePassword.innerHTML = this.value; 
+}
+
+function generationPassword() {
+    let pass = '';
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerPassword.classList.remove('hide');
+    passwoord.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword () {
+    navigator.clipboard.writeText(novaSenha).then(() => {
+        alert('Senha copiada com sucesso!');
+    })
+}  
